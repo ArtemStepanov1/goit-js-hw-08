c2e7c8bf2f361906462e80420b0312dd514be9d4
ArtemStepanov1 <sparta.start@gmail.com>
2022-06-10 21:57:30 +0300

    upd video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+    
+let savedCurrentTime = 0;
+populateTime();
+
+const onPlay = function(data) {            
+    localStorage.setItem("videoplayer-current-time", data.seconds);      
+};
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function populateTime() {
+    const currentTime = localStorage.getItem("videoplayer-current-time");
+    if (currentTime) {        
+        savedCurrentTime = currentTime;
+    }
+}
+
+ player.setCurrentTime(savedCurrentTime);
